810cbdf9dbfcdc96ccff399e6803ad573d71f723
mikemajara <fmiguel05@gmail.com>
2022-04-06 02:23:37 +0200

    chore: checkpoint

--- bot.js
+++ bot.js
@@ -11,14 +11,86 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const grammy_1 = require("grammy");
 const menu_1 = require("@grammyjs/menu");
+const storage_supabase_1 = require("@grammyjs/storage-supabase");
+const supabase_js_1 = require("@supabase/supabase-js");
+const stateless_question_1 = require("@grammyjs/stateless-question");
+const URL = "https://zpwcqzexvdiqicbjhdeh.supabase.co";
+const KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwd2NxemV4dmRpcWljYmpoZGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkxOTU3MzYsImV4cCI6MTk2NDc3MTczNn0.wW_ILnhJ68TK0_nr11nOA2giRKpwS_OcMNeVU-8e4_M";
+// supabase instance
+const supabase = (0, supabase_js_1.createClient)(URL, KEY);
+// Create an instance of the `Bot` class and pass your authentication token to it.
+const bot = new grammy_1.Bot("5288372022:AAHT_fV-9n8nY_SdevcvbWLcLh-g7WFZQkY"); // <-- put your authentication token between the ""
+// create storage
+const storage = (0, storage_supabase_1.supabaseAdapter)({
+    supabase,
+    table: "sessions", // the defined table name you want to use to store your session
+});
+// Create bot and register session middleware
+bot.use((0, grammy_1.session)({
+    initial: () => ({ list: [], title: "Untitled" }),
+    storage,
+    // getSessionKey: (ctx: Context): string | undefined =>
+    //   `${ctx.chat?.id}${ctx.message?.message_id}`,
+}));
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// Create a simple menu.
+const menu = new menu_1.Menu("list")
+    .text("👍", (ctx) => {
+    let name = getName(ctx);
+    ctx.session.list = ctx.session.list.concat(name);
+    ctx.editMessageText(formatNames(ctx));
+})
+    .text("👎", (ctx) => {
+    let name = getName(ctx);
+    ctx.session.list = ctx.session.list.filter((e) => e !== name && !new RegExp(`\(${name}\)`, "gm").test(e));
+    ctx.editMessageText(formatNames(ctx));
+})
+    .text("Invitado", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    yield inviteQuestion.replyWithMarkdown(ctx, "Como se llama tu invitado?" +
+        inviteQuestion.messageSuffixMarkdown());
+}));
+// Make it interactive.
+bot.use(menu);
+const titleQuestion = new stateless_question_1.StatelessQuestion("title", (ctx) => {
+    var _a, _b;
+    ctx.session.title = (_b = (_a = ctx.message) === null || _a === void 0 ? void 0 : _a.text) !== null && _b !== void 0 ? _b : ctx.session.title;
+    ctx.reply(formatNames(ctx), {
+        reply_markup: menu,
+    });
+});
+const inviteQuestion = new stateless_question_1.StatelessQuestion("invite", (ctx) => {
+    var _a;
+    ctx.session.list = ctx.session.list.concat(`${(_a = ctx.message) === null || _a === void 0 ? void 0 : _a.text} (${getName(ctx)})`);
+    ctx.reply(formatNames(ctx), {
+        reply_markup: menu,
+    });
+});
+bot.use(titleQuestion.middleware());
+bot.use(inviteQuestion.middleware());
+bot.command("newlist", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    // Send the menu.
+    yield titleQuestion.replyWithMarkdown(ctx, "Give your list a title" + titleQuestion.messageSuffixMarkdown());
+    // await ctx.reply(`${ctx.session.title}\n\n`, {
+    //   reply_markup: menu,
+    // });
+}));
+// Handle other messages.
+// bot.on("message", (ctx) => ctx.reply("Got another message!"));
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+// Start the bot.
+bot.start();
 const getName = (ctx) => {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
-    let first_name = (_b = (_a = ctx.message) === null || _a === void 0 ? void 0 : _a.text) === null || _b === void 0 ? void 0 : _b.includes((_d = (_c = ctx.from) === null || _c === void 0 ? void 0 : _c.first_name) !== null && _d !== void 0 ? _d : "");
-    let last_name = (_f = (_e = ctx.message) === null || _e === void 0 ? void 0 : _e.text) === null || _f === void 0 ? void 0 : _f.includes((_h = (_g = ctx.from) === null || _g === void 0 ? void 0 : _g.last_name) !== null && _h !== void 0 ? _h : "");
-    if (!(first_name || last_name)) {
-        return (_k = (_j = ctx.from) === null || _j === void 0 ? void 0 : _j.username) !== null && _k !== void 0 ? _k : "unknown";
+    var _a, _b, _c, _d, _e, _f;
+    let first_name = (_b = (_a = ctx.from) === null || _a === void 0 ? void 0 : _a.first_name) !== null && _b !== void 0 ? _b : "";
+    let last_name = (_d = (_c = ctx.from) === null || _c === void 0 ? void 0 : _c.last_name) !== null && _d !== void 0 ? _d : "";
+    if (first_name && last_name) {
+        return first_name + " " + last_name;
     }
-    return first_name + " " + last_name;
+    return (_f = (_e = ctx.from) === null || _e === void 0 ? void 0 : _e.username) !== null && _f !== void 0 ? _f : "unkown";
 };
 const getMessage = (ctx) => {
     var _a, _b;
@@ -28,43 +100,7 @@ const isNameInList = (ctx) => {
     let name = getName(ctx);
     return ctx.session.list.includes(name);
 };
-// Create an instance of the `Bot` class and pass your authentication token to it.
-const bot = new grammy_1.Bot("5288372022:AAHT_fV-9n8nY_SdevcvbWLcLh-g7WFZQkY"); // <-- put your authentication token between the ""
-// Install session middleware, and define the initial session value.
-function initial() {
-    return { list: [] };
-}
-bot.use((0, grammy_1.session)({ initial }));
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Create a simple menu.
-const menu = new menu_1.Menu("my-menu-identifier").text((ctx) => {
-    return isNameInList(ctx) ? "Remove me" : "Add me";
-}, (ctx) => {
-    let message = getMessage(ctx);
-    let name = getName(ctx);
-    console.log(`message: ${message}`);
-    console.log(`name: ${name}`);
-    if (isNameInList(ctx)) {
-        ctx.session.list = ctx.session.list.filter((e) => e !== name);
-    }
-    else {
-        ctx.session.list.push(name);
-    }
+const formatNames = (ctx) => {
     const preparedNames = ctx.session.list.map((e, i) => `${i + 1}. ${e}`);
-    ctx.editMessageText(`Lista\n\n${preparedNames.join("\n")}\n`);
-});
-// Make it interactive.
-bot.use(menu);
-bot.command("list", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    // Send the menu.
-    yield ctx.reply("Lista\n\n", { reply_markup: menu });
-}));
-// Handle other messages.
-// bot.on("message", (ctx) => ctx.reply("Got another message!"));
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
-// Start the bot.
-bot.start();
+    return `${ctx.session.title}\n\n${preparedNames.join("\n")}\n`;
+};
--- bot.ts
+++ bot.ts
@@ -1,25 +1,124 @@
 import { Bot, Context, session, SessionFlavor } from "grammy";
 import { Menu } from "@grammyjs/menu";
+import { supabaseAdapter } from "@grammyjs/storage-supabase";
+import { createClient } from "@supabase/supabase-js";
+import { StatelessQuestion } from "@grammyjs/stateless-question";
 
 // Define the shape of our session.
 interface SessionData {
+  title: string;
   list: string[];
 }
 
 // Flavor the context type to include sessions.
 type MyContext = Context & SessionFlavor<SessionData>;
 
+const URL = "https://zpwcqzexvdiqicbjhdeh.supabase.co";
+const KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwd2NxemV4dmRpcWljYmpoZGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkxOTU3MzYsImV4cCI6MTk2NDc3MTczNn0.wW_ILnhJ68TK0_nr11nOA2giRKpwS_OcMNeVU-8e4_M";
+
+// supabase instance
+const supabase = createClient(URL, KEY);
+
+// Create an instance of the `Bot` class and pass your authentication token to it.
+const bot = new Bot<MyContext>(
+  "5288372022:AAHT_fV-9n8nY_SdevcvbWLcLh-g7WFZQkY",
+); // <-- put your authentication token between the ""
+
+// create storage
+const storage = supabaseAdapter({
+  supabase,
+  table: "sessions", // the defined table name you want to use to store your session
+});
+
+// Create bot and register session middleware
+bot.use(
+  session({
+    initial: () => ({ list: [], title: "Untitled" }),
+    storage,
+    // getSessionKey: (ctx: Context): string | undefined =>
+    //   `${ctx.chat?.id}${ctx.message?.message_id}`,
+  }),
+);
+
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+// Create a simple menu.
+const menu = new Menu<MyContext>("list")
+  .text("👍", (ctx: MyContext) => {
+    let name = getName(ctx);
+    ctx.session.list = ctx.session.list.concat(name);
+    ctx.editMessageText(formatNames(ctx));
+  })
+  .text("👎", (ctx: MyContext) => {
+    let name = getName(ctx);
+    ctx.session.list = ctx.session.list.filter(
+      (e: string) =>
+        e !== name && !new RegExp(`\(${name}\)`, "gm").test(e),
+    );
+    ctx.editMessageText(formatNames(ctx));
+  })
+  .text("Invitado", async (ctx: MyContext) => {
+    await inviteQuestion.replyWithMarkdown(
+      ctx,
+      "Como se llama tu invitado?" +
+        inviteQuestion.messageSuffixMarkdown(),
+    );
+  });
+
+// Make it interactive.
+bot.use(menu);
+
+const titleQuestion = new StatelessQuestion("title", (ctx: any) => {
+  ctx.session.title = ctx.message?.text ?? ctx.session.title;
+  ctx.reply(formatNames(ctx), {
+    reply_markup: menu,
+  });
+});
+
+const inviteQuestion = new StatelessQuestion("invite", (ctx: any) => {
+  ctx.session.list = ctx.session.list.concat(
+    `${ctx.message?.text} (${getName(ctx)})`,
+  );
+  ctx.reply(formatNames(ctx), {
+    reply_markup: menu,
+  });
+});
+
+bot.use(titleQuestion.middleware());
+bot.use(inviteQuestion.middleware());
+
+bot.command("newlist", async (ctx: MyContext) => {
+  // Send the menu.
+  await titleQuestion.replyWithMarkdown(
+    ctx,
+    "Give your list a title" + titleQuestion.messageSuffixMarkdown(),
+  );
+  // await ctx.reply(`${ctx.session.title}\n\n`, {
+  //   reply_markup: menu,
+  // });
+});
+
+// Handle other messages.
+// bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+
+// Start the bot.
+bot.start();
+
 const getName = (ctx: Context) => {
-  let first_name = ctx.message?.text?.includes(
-    ctx.from?.first_name ?? "",
-  );
-  let last_name = ctx.message?.text?.includes(
-    ctx.from?.last_name ?? "",
-  );
-  if (!(first_name || last_name)) {
-    return ctx.from?.username ?? "unkown";
+  let first_name = ctx.from?.first_name ?? "";
+  let last_name = ctx.from?.last_name ?? "";
+  if (first_name && last_name) {
+    return first_name + " " + last_name;
   }
-  return first_name + " " + last_name;
+  return ctx.from?.username ?? "unkown";
 };
 
 const getMessage = (ctx: Context) => {
@@ -31,60 +130,9 @@ const isNameInList = (ctx: MyContext) => {
   return ctx.session.list.includes(name);
 };
 
-// Create an instance of the `Bot` class and pass your authentication token to it.
-const bot = new Bot<MyContext>(
-  "5288372022:AAHT_fV-9n8nY_SdevcvbWLcLh-g7WFZQkY",
-); // <-- put your authentication token between the ""
-
-// Install session middleware, and define the initial session value.
-function initial(): SessionData {
-  return { list: [] };
-}
-bot.use(session({ initial }));
-
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
-
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-
-// Create a simple menu.
-const menu = new Menu("my-menu-identifier").text(
-  (ctx: any) => {
-    return isNameInList(ctx) ? "Remove me" : "Add me";
-  },
-  (ctx: any) => {
-    let message = getMessage(ctx);
-    let name = getName(ctx);
-    console.log(`message: ${message}`);
-    console.log(`name: ${name}`);
-    if (isNameInList(ctx)) {
-      ctx.session.list = ctx.session.list.filter(
-        (e: string) => e !== name,
-      );
-    } else {
-      ctx.session.list.push(name);
-    }
-    const preparedNames = ctx.session.list.map(
-      (e: any, i: any) => `${i + 1}. ${e}`,
-    );
-    ctx.editMessageText(`Lista\n\n${preparedNames.join("\n")}\n`);
-  },
-);
-
-// Make it interactive.
-bot.use(menu);
-
-bot.command("list", async (ctx) => {
-  // Send the menu.
-  await ctx.reply("Lista\n\n", { reply_markup: menu });
-});
-
-// Handle other messages.
-// bot.on("message", (ctx) => ctx.reply("Got another message!"));
-
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
-
-// Start the bot.
-bot.start();
+const formatNames = (ctx: MyContext) => {
+  const preparedNames = ctx.session.list.map(
+    (e: any, i: any) => `${i + 1}. ${e}`,
+  );
+  return `${ctx.session.title}\n\n${preparedNames.join("\n")}\n`;
+};
